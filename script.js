f1db8c101f5d89b425291019185baeb764b1698c
sachinsherekar <130824306+sachinsherekar@users.noreply.github.com>
2023-06-03 14:43:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 // complete the given function
-
-function palindrome(str){
-
+function palindrome(str) {
+  var a = str.replace(/[^\w\s]|_/g, "").toLowerCase().replace(/\s/g, "");
+  if (a === a.split("").reverse().join("")) {
+    return true;
+  }
+  return false;
 }
 module.exports = palindrome
+
+
